4a4d193dbd1160fb71fa470c34e92afbe0d31a23
mohamedsaeed <mohamedsaeed00451@gmail.com>
2023-01-31 15:58:26 +0200

    Update This File

--- ROSHETTA_API/API_Admin/Delete_Place.php
+++ ROSHETTA_API/API_Admin/Delete_Place.php
@@ -1,56 +1,39 @@
 <?php
 
 require_once("../API_C_A/Allow.php"); //Allow All Headers
+require_once("../API_C_A/Connection.php"); //Connect To DataBases
 
 session_start();
 session_regenerate_id();
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST' || isset($_SESSION['admin'])) { //Allow Access Via 'POST' Method Or Admin
 
-    if (isset($_SESSION['admin'])) {
-
-        require_once("../API_C_A/Connection.php"); //Connect To DataBases
+    if (isset($_SESSION['admin'])) {  //If Admin
 
         if (isset($_POST['clinic_id']) && !empty($_POST['clinic_id'])) {
-
-            $clinic_id = filter_var($_POST['clinic_id'], FILTER_SANITIZE_NUMBER_INT);
-
-            // Delete From Clinic Table
-
-            $delete_clinic = $database->prepare("DELETE FROM clinic WHERE clinic.id = :clinic_id");
-            $delete_clinic->bindparam("clinic_id", $clinic_id);
-            $delete_clinic->execute();
-
-            if ($delete_clinic->rowCount() > 0) {
-
-                print_r(json_encode(["Message" => "تم الحذف بنجاح"]));
-
-            } else {
-                print_r(json_encode(["Error" => "فشل الحذف"]));
-            }
-
+            $id         = filter_var($_POST['clinic_id'], FILTER_SANITIZE_NUMBER_INT);
+            $table_name = 'clinic';
         } elseif (isset($_POST['pharmacy_id']) && !empty($_POST['pharmacy_id'])) {
-
-            $pharmacy_id = filter_var($_POST['pharmacy_id'], FILTER_SANITIZE_NUMBER_INT);
-
-            // Delete From Pharmacy Table
-
-            $delete_pharmacy = $database->prepare("DELETE FROM pharmacy WHERE pharmacy.id = :pharmacy_id");
-            $delete_pharmacy->bindparam("pharmacy_id", $pharmacy_id);
-            $delete_pharmacy->execute();
-
-            if ($delete_pharmacy->rowCount() > 0) {
-
-                print_r(json_encode(["Message" => "تم الحذف بنجاح"]));
-
-            } else {
-                print_r(json_encode(["Error" => "فشل الحذف"]));
-            }
-
+            $id         = filter_var($_POST['pharmacy_id'], FILTER_SANITIZE_NUMBER_INT);
+            $table_name = 'pharmacy';
         } else {
-            print_r(json_encode(["Error" => "فشل العثور على المكان"]));
+            $id = '';
+            $table_name = '';
         }
 
+        // Delete From Place Table
+
+        $delete_place = $database->prepare("DELETE FROM $table_name WHERE id = :id");
+        $delete_place->bindparam("id", $id);
+        $delete_place->execute();
+
+        if ($delete_place->rowCount() > 0) {
+
+            print_r(json_encode(["Message" => "تم الحذف بنجاح"]));
+
+        } else {
+            print_r(json_encode(["Error" => "فشل الحذف"]));
+        }
     } else {
         print_r(json_encode(["Error" => "ليس لديك الصلاحية"]));
     }
