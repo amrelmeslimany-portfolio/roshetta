658e5e3edf9801951483b7d8785adc5393b23955
amrelmeslimany-portfolio <aamr_786@hotmail.com>
2023-02-11 21:22:34 +0200

    Init Flutter App

--- ROSHETTA_API/API_User/Menu_User/Menu_Doctor/View_Data_Clinic.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once("../../../API_C_A/Allow.php"); //Allow All Headers
-require_once("../../../API_C_P/Clinic/View_Data.php"); //View_Data_Clinic  
-require_once("../../../API_Function/All_Function.php"); //All Function
-?>
