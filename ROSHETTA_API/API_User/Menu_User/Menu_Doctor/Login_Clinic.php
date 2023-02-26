7be931939e92bd2250b270b9531bb3a5f42b708d
mohamedsaeed <mohamedsaeed00451@gmail.com>
2023-02-26 20:02:23 +0200

    m

--- ROSHETTA_API/API_User/Menu_User/Menu_Doctor/Login_Clinic.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once("../../../API_C_A/Allow.php"); //Allow All Headers
-require_once("../../../API_C_A/Connection.php"); //Connect To DataBase
-require_once("../../../API_Function/All_Function.php"); //All Function
-require_once("../../../API_C_P/Clinic/Login.php"); //Login_Clinic 
-date_default_timezone_set('Africa/Cairo'); //Set To Cairo TimeZone 
-?>
--- ROSHETTA_API/API_User/Menu_User/Menu_Doctor/Logout_Clinic.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once("../../../API_C_A/Allow.php"); //Allow All Headers 
-require_once("../../../API_Function/All_Function.php"); //All Function
-require_once("../../../API_C_P/Logout_C_P.php"); //Logout_Clinic 
-?>
