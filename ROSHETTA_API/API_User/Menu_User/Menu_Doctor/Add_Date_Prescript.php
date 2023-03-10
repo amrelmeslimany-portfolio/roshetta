8cd9ae6bd9078389d05f9c6b4168d5bf173d90cc
amrelmeslimany-portfolio <aamr_786@hotmail.com>
2023-03-10 15:14:14 +0200

    Clear Front branch

--- ROSHETTA_API/API_User/Menu_User/Menu_Doctor/Add_Date_Prescript.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once("../../../API_C_A/Allow.php"); //Allow All Headers
-require_once("../../../API_C_A/Connection.php"); //Connect To DataBase
-require_once("../../../API_D_P_M/Registration_p.php"); //Registration_Prescript  
-?>
--- ROSHETTA_API/API_User/Menu_User/Menu_Pharmacist/Add_Pharmacy.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once("../../../API_C_A/Allow.php"); //Allow All Headers
-require_once("../../../API_C_A/Connection.php"); //Connect To DataBase
-require_once("../../../API_C_P/Pharmacy/Registration.php"); //Registration_Pharmacy  
-?>
