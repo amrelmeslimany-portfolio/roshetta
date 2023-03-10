8b86d6c32fc5cb405e6399d14fb49a4b44067f0c
amrelmeslimany-portfolio <aamr_786@hotmail.com>
2023-03-10 15:53:36 +0200

    Init front end roshetta app
    Ditribute Tasks

---
+++ roshetta/src/App.js
@@ -0,0 +1,31 @@
+import "./styles/app.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>تطبيق روشته</h1>
+      <p>
+        التطبيق بالعربي <br /> هتشتغلو بمكتبه <b>antd</b> حاجه زي البوتستراب كد
+        بس فيها المكونات كلها <br /> هتشتغلو بخط اسمه <b>Cairo</b>
+      </p>
+      <ul>
+        <li>
+          <h2>محمد الحسن</h2>
+          <ul>
+            <li>صفحات تسجيل الدخول (pages/auth/)</li>
+            <li>صفحات الادمن (pages/admin)</li>
+            <li>صفحات الدكتور (pages/doctor)</li>
+          </ul>
+        </li>
+        <li>
+          <h2>محمد عثمان</h2>
+          <ul>
+            <li>صفحات المريض (pages/patient/)</li>
+            <li>صفحات الصيدلي (pages/pharmcist)</li>
+            <li>صفحات المساعد (pages/assistant)</li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  );
+}
