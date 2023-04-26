2c70a26ca2895f6af4b57c855101b8acb44aa8b5
Muhammad El Hassan <mohammedelhassan2001@gmail.com>
2023-04-26 12:41:45 +0200

    ReadMe Edit

--- src/pages/Doctor/components/DoctorHeader.jsx
+++ src/pages/Doctor/components/DoctorHeader.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import images from '../../../images';
 
 const DoctorHeader = () => {
-  return <div className='doctor__header'>DoctorHeader</div>;
+  return (
+    <div
+      //  className="doctor__header "
+      className="flex justify-between items-center border-b-2 p-1.5"
+    >
+      <div className="text-green-600 text-xl font-extrabold">30107231801999</div>
+      <img className="object-contain w-40 h-10" src={images.logo2} alt="" />
+    </div>
+  );
 };
 
-export default DoctorHeader;
+export default DoctorHeader; 
