cb3ae7b6c050a594b5969e8ae71d93389d90bdcf
Muhammad El Hassan <mohammedelhassan2001@gmail.com>
2023-05-15 12:05:49 +0300

    Fixed Bugs

--- src/pages/auth/ActiveEmail/ActiveEmail.jsx
+++ src/pages/auth/ActiveEmail/ActiveEmail.jsx
@@ -3,8 +3,9 @@ import './ActiveEmail.scss';
 import { MdEmail } from 'react-icons/md';
 import { Alert } from 'antd';
 import { useGlobalContext } from '../../../context';
-import { useNavigate } from 'react-router-dom';
+import { Navigate, useNavigate } from 'react-router-dom';
 import { AppWrapper } from '../../../wrapper';
+import { MyLoader } from '../../../components';
 
 const ActiveEmail = () => {
   const { setAuthUser, alert, setAlert } = useGlobalContext();
@@ -14,14 +15,14 @@ const ActiveEmail = () => {
   const [code, setCode] = useState('');
   const [role, setRole] = useState('');
   const [email, setEmail] = useState('');
-
+  const [loading, setLoading] = useState(false);
   const registerData = JSON.parse(localStorage.getItem('registerData'));
 
   let formData = new FormData();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
+    setLoading(true);
     console.log(registerData);
     setRole(registerData[0]);
     setEmail(registerData[1]);
@@ -50,9 +51,8 @@ const ActiveEmail = () => {
       })
         .then((res) => res.json())
         .then((data) => {
-          console.log(data);
-          setAuth(data.Status);
           if (data.Status > 299) {
+            console.log(data);
             window.scrollTo({
               top: 0,
               left: 0,
@@ -63,9 +63,11 @@ const ActiveEmail = () => {
               show: true,
               type: 'error',
             });
+            setLoading(false);
           } else {
-            console.log(data.Message);
+            console.log(data);
             localStorage.setItem('message', JSON.stringify([data.Message]));
+            setLoading(false);
             navigate('/login');
           }
         });
@@ -80,46 +82,56 @@ const ActiveEmail = () => {
       clearTimeout(myTimeout);
     };
   }, [alert.show]);
+  loading;
+  if (loading) {
+    return <MyLoader text={'جاري تفعيل الحساب...'} loading={loading} />;
+  }
   return (
     <>
-      {alert.show && (
-        <Alert
-          style={{
-            marginBottom: 20,
-          }}
-          message="حدثت مشكلة"
-          description={alert.msg}
-          type={alert.type}
-          showIcon
-        />
-      )}
-      <div className="h-screen mx-auto flex-col gap-6 flex justify-center items-center">
+      <div className="mx-auto flex h-screen flex-col items-center justify-center gap-6">
+        {alert.show && (
+          <motion.div
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}
+          >
+            <Alert
+              style={{
+                marginBottom: 20,
+              }}
+              message="حدثت مشكلة"
+              description={alert.msg}
+              type={alert.type}
+              showIcon
+            />
+          </motion.div>
+        )}
         <div className="text-8xl text-roshetta">
           <MdEmail />
         </div>
         <h2 className="text-2xl font-extrabold text-roshetta">
           تم ارسال كود الى الايميل الخاص بك
         </h2>
-        <p className="text-xl text-[#a7a7a7] mb-4">
+        <p className="mb-4 text-xl text-[#a7a7a7]">
           تم ارسال كود الي بريدك الالكتروني بنجاح
         </p>
-        <p className="text-xl text-[#a7a7a7] mb-4">
+        <p className="mb-4 text-xl text-[#a7a7a7]">
           قم باستخدام هذا الكود لتأكيد حسابك وتسجيل الدخول.
         </p>
         <form
-          className="flex flex-col justify-center items-center gap-6"
+          className="flex flex-col items-center justify-center gap-6"
           onSubmit={handleSubmit}
         >
           <input
             onChange={(e) => setCode(e.target.value)}
             value={code}
-            className="px-2 py-1 border-2 border-slate-300 focus:outline-none rounded-2xl w-96 h-14 text-2xl "
+            className="h-14 w-96 rounded-2xl border-2 border-slate-300 px-2 py-1 text-2xl focus:outline-none "
             type="text"
             placeholder="ادخل الكود"
           />
           <button
+            className="foucs:outline-2 mt-6 rounded-full bg-roshetta px-40 py-3 text-2xl text-white hover:bg-green-500 focus:outline-none focus:ring focus:ring-green-300  active:bg-green-600"
             type="submit"
-            className="bg-roshetta text-white px-6 py-2 rounded-full hover:text-lg transition-all"
           >
             تأكيد الحساب
           </button>
