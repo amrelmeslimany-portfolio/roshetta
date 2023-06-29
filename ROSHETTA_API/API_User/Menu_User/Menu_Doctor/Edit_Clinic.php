325334b0d2fa38714960a1bbf92b60f4d1ebaef8
amrelmeslimany-portfolio <93720065+amrelmeslimany-portfolio@users.noreply.github.com>
2023-06-29 21:43:16 +0300

    Delete ROSHETTA_API directory

--- ROSHETTA_API/API_User/Menu_User/Menu_Doctor/Edit_Clinic.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once("../../../API_C_A/Allow.php"); //Allow All Headers
-require_once("../../../API_C_A/Connection.php"); //Connect To DataBase
-require_once("../../../API_Function/All_Function.php"); //All Function
-require_once("../../../API_C_P/Clinic/Edit.php"); //Edit_Clinic  
-?>
